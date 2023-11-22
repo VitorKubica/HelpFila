a76e5641ef88c7bd9668acdd4b82e9386ba6f28d
nichol6s <nicholas.jake15@gmail.com>
2023-11-22 09:50:34 -0300

    Página de login e register criadas

---
+++ src/app/components/Cabecalho/Cabecalho.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function Cabecalho() {
+  return (
+    <div>Cabecalho</div>
+  )
+}
--- src/app/login/page.jsx
+++ src/app/login/page.jsx
@@ -2,11 +2,10 @@ import Link from "next/link"
 
 export default function Login() {
   return (
-
     <div>
         <div>
             <form class="form mx-auto mt-8 p-6 bg-white rounded-lg max-w-md">
-                <p class="title text-2xl text-royalblue font-semibold mb-4">Register</p>
+                <p class="title text-2xl text-royalblue font-semibold mb-4">Login</p>
                 <p class="message text-gray-700 text-sm mb-4">Signup now and get full access to our app.</p>
 
                 <div class="flex mb-4">
@@ -38,10 +37,9 @@ export default function Login() {
 
                 <button class="submit bg-royalblue text-white p-2 rounded-full text-xl transition duration-300 ease-in-out transform hover:bg-blue-600 focus:outline-none focus:shadow-outline-blue active:scale-95">Submit</button>
 
-                <p class="signin mt-4 text-gray-700 text-sm">Already have an account?  <Link href="./login" class="text-royalblue">Sign in</Link></p>
+                <p class="signin mt-4 text-gray-700 text-sm">Already have an account?  <Link href="./" class="text-royalblue">Login</Link></p>
             </form>
         </div>
     </div>
-
   )
 }
