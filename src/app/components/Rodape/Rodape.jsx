f5f32f4f83d331f8be617fc4ec0450ab310581c4
nichol6s <nicholas.jake15@gmail.com>
2023-11-23 00:08:23 -0300

    Organizando ainda mais o projeto e adicionando wave svg

--- src/app/components/Rodape/Rodape.jsx
+++ src/app/components/Rodape/Rodape.jsx
@@ -2,31 +2,33 @@ import { FaFacebook, FaTwitter, FaInstagram } from "react-icons/fa";
 import React from 'react';
 import "../../css/rodape.scss";
 import Link from 'next/link';
+import Image from "next/image";
 
 export default function Rodape() {
   return (
     <footer>
+      <div className="row">
 
-        <div className="row">
-          <div className="Info">
-            <h2>Promovendo mais saúde e menos tempo perdido nas filas.</h2>
-            <p>Faça parte dessa transformação conosco e adote o HelpFila para uma gestão de filas inteligente e humanizada.</p>
-            <div className="info-a ">
-              <Link href="https://github.com/VitorKubica/HelpFila" className="link_info">👀 Code</Link>
-              <Link href="https://github.com/VitorKubica/HelpFila" className="link_info">👋 Contact</Link>
-            </div>
-          </div>
-          <div className="SM">
-            <img src="/logo.png" alt="Logo HelpFila" />
-            <p>@ 2023 HelpFila. All Rights Reserved.</p>
-            <div className="socialMedia">
-              <Link href="" className="link_sm"><FaFacebook style={{ color: "white" }} /></Link>
-              <Link href="" className="link_sm"><FaTwitter style={{ color: "white" }} /></Link>
-              <Link href="" className="link_sm"><FaInstagram style={{ color: "white", }} /></Link>
-            </div>
+        <div className="Info">
+          <h2>Promovendo mais saúde e menos tempo perdido nas filas.</h2>
+          <p>Faça parte dessa transformação conosco e adote o HelpFila para uma gestão de filas inteligente e humanizada.</p>
+          <div className="info-a ">
+            <Link href="https://github.com/VitorKubica/HelpFila" target="_blank" className="link_info">👀 Code</Link>
+            <Link href="https://github.com/VitorKubica/HelpFila" target="_blank" className="link_info">👋 Contact</Link>
           </div>
         </div>
-     
+        <div className="SM">
+          <img src="/logo.png" alt="Logo HelpFila" />
+          <p>@ 2023 HelpFila. All Rights Reserved.</p>
+          <div className="socialMedia">
+            <Link href="" className="link_sm"><FaFacebook style={{ color: "white" }} /></Link>
+            <Link href="" className="link_sm"><FaTwitter style={{ color: "white" }} /></Link>
+            <Link href="" className="link_sm"><FaInstagram style={{ color: "white", }} /></Link>
+          </div>
+
+        </div>
+      </div>
+
     </footer>
   )
 }
