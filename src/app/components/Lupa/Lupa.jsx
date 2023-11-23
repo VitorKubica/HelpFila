b9a997350d301ec19321cab81755c5a700817ec0
nichol6s <nicholas.jake15@gmail.com>
2023-11-23 20:15:06 -0300

    Adicionando a barra de pesquisa com autosearch, arrumando a imagem principal da home tbm

--- src/app/components/Lupa/Lupa.jsx
+++ src/app/components/Lupa/Lupa.jsx
@@ -1,43 +1,53 @@
-import "./Lupa.scss"
+"use client";
+import { useState, useEffect } from 'react';
+import styles from './Lupa.scss'; // Importando estilos do arquivo SASS
+import Input from './Input';
+
+const suggestions = [
+  'Canal',
+  'YouTube',
+  'YouTuber',
+  'Carros',
+  'Facebook',
+  'Dev Sandrin',
+  'Projetos CSS',
+];
 
 export default function Lupa() {
-  function autocomplete() {
-    var input = document.getElementById('search__input');
-    var ul = document.getElementById('myUL');
-    var li, txtValue;
-  
-    // Clear previous suggestions
-    ul.innerHTML = '';
-  
-    // Get the input value
-    var inputValue = input.value.toLowerCase();
-  
-    // You can replace this array with your own data source
-    var suggestions = ['Apple', 'Banana', 'Orange', 'Mango', 'Grapes'];
-  
-    // Filter suggestions based on input
-    suggestions.forEach(function (item) {
-      if (item.toLowerCase().indexOf(inputValue) > -1) {
-        li = document.createElement('li');
-        txtValue = document.createTextNode(item);
-        li.appendChild(txtValue);
-        ul.appendChild(li);
-      }
-    });
-  }
-  
+  const [userData, setUserData] = useState('');
+  const [active, setActive] = useState(false);
+  const [filteredSuggestions, setFilteredSuggestions] = useState([]);
+
+  const handleInputChange = (e) => {
+    const input = e.target.value;
+    setUserData(input);
+
+    const filtered = suggestions.filter((data) =>
+      data.toLowerCase().startsWith(input.toLowerCase())
+    );
+    setFilteredSuggestions(filtered);
+    setActive(!!input); // Ativa sugestões apenas se houver texto no input
+  };
+
+  const generateSearchLink = () => {
+    const link = `https://www.google.com/search?q=${userData}`;
+    window.open(link, '_blank');
+  };
+
+  const selectSuggestion = (suggestion) => {
+    setUserData(suggestion);
+    setActive(false);
+  };
+
   return (
-  <div>
-    <div className="search">
-    <input type="text" className="search__input" placeholder="Pesquisar..." onkeyup="autocomplete()"/>
-    <button className="search__button" >
-        <svg className="search__icon" aria-hidden="true" viewBox="0 0 24 24">
-            <g>
-                <path d="M21.53 20.47l-3.66-3.66C19.195 15.24 20 13.214 20 11c0-4.97-4.03-9-9-9s-9 4.03-9 9 4.03 9 9 9c2.215 0 4.24-.804 5.808-2.13l3.66 3.66c.147.146.34.22.53.22s.385-.073.53-.22c.295-.293.295-.767.002-1.06zM3.5 11c0-4.135 3.365-7.5 7.5-7.5s7.5 3.365 7.5 7.5-3.365 7.5-7.5 7.5-7.5-3.365-7.5-7.5z"></path>
-            </g>
-        </svg>
-    </button>
+    <div className={styles.container}>
+      <div className={`${styles.search} ${active ? styles.active : ''}`}>
+        <a href={`https://www.google.com/search?q=${userData}`} target="_blank" rel="noopener noreferrer" hidden></a>
+        <Input/>
+        <div className={styles.icon} onClick={generateSearchLink}>
+          <ion-icon name="search-outline"></ion-icon>
+        </div>
+      </div>
     </div>
-    </div>
-  )
+  );
 }
