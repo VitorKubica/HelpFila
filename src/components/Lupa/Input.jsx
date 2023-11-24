638dfc351c28e6b5e7a341f38c9f5ec115ef4232
VitorKubica <vitor05kubica12@gmail.com>
2023-11-24 17:46:05 -0300

    estilo barra de pesquisa

--- src/components/Lupa/Input.jsx
+++ src/components/Lupa/Input.jsx
@@ -1,7 +1,8 @@
 import { CiSearch } from "react-icons/ci";
 import { useState } from "react";
 import Link from "next/link";
- 
+import style from "./input.scss"
+
 const suggestions = [
   "Canal",
   "YouTube",
@@ -16,6 +17,7 @@ export default function Input() {
   const [inputValue, setInputValue] = useState("");
   const [filteredOptions, setFilteredOptions] = useState(suggestions);
   const [isFocused, setIsFocused] = useState(false);
+  const [removeBorderRadius, setRemoveBorderRadius] = useState(false);
  
  
   const handleInputChange = (e) => {
@@ -37,31 +39,33 @@ export default function Input() {
  
   const handleInputFocus = () => {
     setIsFocused(true);
+    setRemoveBorderRadius(true);
   };
- 
+
   const handleInputBlur = () => {
     setIsFocused(false);
+    setRemoveBorderRadius(false);
   };
  
   return (
     <div className="flex mt-5 justify-center">
       <div className=" w-1/3  flex justify-center flex-col search ">
-          <div className="flex justify-around px-3 items-center bg-white w-full shadow appearance-none  rounded-full h-10 " >
+          <div className={`flex justify-around px-3 items-center bg-white w-full shadow appearance-none  rounded-full h-10  ${removeBorderRadius ? 'no-border-radius' : '' }`}>
             <CiSearch />
             <input
               type="text"
               name="searchInput"
               id="searchInput"
               placeholder="Pesquisar..."
-              className=" w-full mx-3 focus:outline-none focus:shadow-outline"
+              className=" w-full mx-3 focus:outline-none focus:shadow-outline "
               onChange={handleInputChange}
               onFocus={handleInputFocus}
               onBlur={handleInputBlur}
             />
           </div>
- 
+
           {isFocused && (
-            <ul className="bg-white rounded-lg  shadow">
+            <ul className="bg-white rounded-b-3xl  shadow">
               {filteredOptions.map((option, index) => (
                 <li
                   className=" py-1 flex px-2 hover:bg-gray-200 transition-all duration-300"
