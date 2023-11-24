e4517ce17faf5d2ff58f9c36a106da5f9a72b4e3
nichol6s <nicholas.jake15@gmail.com>
2023-11-24 12:23:48 -0300

    Arrumando o caminho de pastas, e estilizando a not-found

--- src/app/not-found.jsx
+++ src/app/not-found.jsx
@@ -7,10 +7,10 @@ export default function Error404() {
       <section className="flex justify-center my-10">
         <Image src="/404.svg" alt="Imagem de erro" width={435} height={435} />
         <div className="flex flex-col items-center text-center justify-around w-80">
-          <h1 className="text-5xl font-bold text-white mb-4">404...</h1>
-          <h2 className="text-2xl mb-2 text-white">Caminho errado!</h2>
-          <h2 className="text-2xl mb-4 text-white">Não encontramos essa página.</h2>
-          <Link href="/home" className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Volte para o início</Link>
+          <h1 className="text-7xl font-bold text-[#12314E]">404...</h1>
+          <h2 className="text-2xl mb-2 text-[#12314E]">Caminho errado!</h2>
+          <h2 className="text-2xl mb-4 text-[#12314E]">Não encontramos essa página.</h2>
+          <Link href="/home" className="bg-[#12314E] hover:bg-[#12318A] text-white font-bold py-2 px-4 rounded">Volte para o início</Link>
         </div>
       </section>
     </div>
