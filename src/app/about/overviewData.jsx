3014b3b0e7daa35ec43bdf030a6f1b7338c2edaa
nichol6s <nicholas.jake15@gmail.com>
2023-11-24 20:16:59 -0300

    Realizando testes com imagens na overviewData

--- src/app/about/overviewData.jsx
+++ src/app/about/overviewData.jsx
@@ -1,7 +1,7 @@
-import ilustracao01 from "../../assets/img/ilustracao1.svg";
-import ilustracao02 from "../../assets/img/ilustracao3.svg";
-import ilustracao03 from "../../assets/img/ilustracao8.svg";
-import ilustracao04 from "../../assets/img/ilustracao7.svg";
+import ilustracao01 from "../../assets/img/teste.png";
+import ilustracao02 from "../../assets/img/teste.png";
+import ilustracao03 from "../../assets/img/teste.png";
+import ilustracao04 from "../../assets/img/teste.png";
 
 export const overviewData = [
     {
