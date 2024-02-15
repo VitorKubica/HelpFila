5bea4fa36858e7ac8b2e9d6851c7c8ef18191270
VitorKubica <vitor05kubica12@gmail.com>
2024-02-15 09:51:05 -0300

    tirei o request do formulário

--- src/app/login/page.jsx
+++ src/app/login/page.jsx
@@ -2,46 +2,8 @@
 import Link from "next/link"
 import "./login.scss"
 import Image from "next/image";
-import { useRef, useState } from "react";
  
 export default function Login() {
- 
-    const [error, setError] = useState("");
-    const email = useRef();
-    const password = useRef();
- 
-    const handleSubmit = async (e) => {
-        e.preventDefault();
-        const emailValue = email.current.value;
-        const passwordValue = password.current.value;
-   
-        try {
-          const response = await fetch("http://localhost:5000/clients");
-          const clients = await response.json();
-          const user = clients.find(
-            (u) => u.email === emailValue && u.senha === passwordValue
-          );
-   
-          if (user) {
-            setError("");
-            console.log("Login bem-sucedido!");
-            let token =
-              Math.random().toString(16).substring(2) +
-              Math.random().toString(16).substring(2);
- 
-            sessionStorage.setItem("userData", user.nome);
-            sessionStorage.setItem("userData", user.email);
-            sessionStorage.setItem("senhaData", token);
-            window.location = "/home";
-          } else {
-            console.log("nao foi!");
-            setError("Credenciais inválidas. Tente novamente.");
-          }
-        } catch (error) {
-          setError("Erro ao fazer login. Tente novamente mais tarde.");
-          console.log("Erro ao fazer login:", error);
-        }
-      };
  
   return (
     <div className="pagina">
@@ -50,18 +12,17 @@ export default function Login() {
         </div>
     <div className="login">
         <div className="container">
-            <form className="form_login" onSubmit={handleSubmit}>
+            <form className="form_login">
                 <p className="title">Login</p>
                 <label>
-                    <input className="input" type="email" placeholder=" Email " ref={email} required/>
+                    <input className="input" type="email" placeholder=" Email " required/>
                 </label>
  
                 <label>
-                    <input className="input" type="password" placeholder=" Senha " ref={password} required/>
+                    <input className="input" type="password" placeholder=" Senha " required/>
                 </label>
  
-                <button className="submit" type="submit">Login</button>
-                {error && <p className="border ite">{error}</p>}
+                <Link href="./home"><button className="submit" type="submit">Login</button></Link>
                 <p className="signin">Ainda não tem uma conta?  <Link href="./">Registre-se</Link></p>
             </form>
             </div>
